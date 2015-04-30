7f582d23d815c73e4f8749e9c4046e07645bbd96
JKrehl <Jonas.Krehl@triebenberg.de>
2015-04-30 11:09:49 +0200

    volume trafos work

--- Utilities/VolumeTrafos.py
+++ Utilities/VolumeTrafos.py
@@ -7,44 +7,35 @@ from scipy import ndimage
 
 from . import CoordinateTrafos as CT
 
-class VTrafo3D:
+class VolumeTrafo3D:
 
-	def __init__(self, trafo, shape, origin='center', units='equal'):
+	def __init__(self, trafo, shape, tshape, origin='center'):
 		assert isinstance(trafo, CT.Trafo3D)
 		self.trafo = trafo.inv
 
 		self.shape = shape
+		self.tshape = tshape
 		
 		self.origin = origin
 
-		if units == 'equal':
-			self.units = numpy.array((1,1,1))
-		elif numpy.isscalar(units):
-			self.units = numpy.array((units,units,units))
-		elif hasattr(units, '__getitem__'):
-			self.units = numpy.require(units)
-		else:
-			raise AttributeError
-
-		z = numpy.arange(self.shape[0])
-		y = numpy.arange(self.shape[1])
-		x = numpy.arange(self.shape[2])
+		self.idxtrafo = self.trafo.copy()
 		
 		if self.origin == 'center':
-			z -= shp[0]//2
-			y -= shp[1]//2
-			x -= shp[2]//2
+			self.idxtrafo.add_preshift([-i//2 for i in self.shape])
+			self.idxtrafo.add_postshift([i//2 for i in self.tshape])
 		elif self.origin == 'zero':
 			pass
 		else:
 			raise AttributeError
 
-		nz, ny, nx, off = self.trafo.apply_to_bases(z,y,x)
+		tzyxo = self.idxtrafo.inv.apply_to_bases(*tuple(numpy.arange(i) for i in self.shape))
+		self.proj_coords = numexpr.evaluate('x+y+z+o', local_dict=dict(z=tzyxo[0][:,:,None,None], y=tzyxo[1][:,None,:,None], x=tzyxo[2][:,None,None,:], o=tzyxo[3][:,None,None,None]))
 
-		self.to_coords = 
-		
+		zyxo = self.idxtrafo.apply_to_bases(*tuple(numpy.arange(i) for i in self.tshape))
+		self.bproj_coords = numexpr.evaluate('x+y+z+o', local_dict=dict(z=zyxo[0][:,:,None,None], y=zyxo[1][:,None,:,None], x=zyxo[2][:,None,None,:], o=zyxo[3][:,None,None,None]))
 
-	def apply_to(self, V):
-
-
-		
+	def apply_to(self, a):
+		return ndimage.interpolation.map_coordinates(a, self.proj_coords, order=1)
+    
+	def apply_fro(self, a):
+		return ndimage.interpolation.map_coordinates(a, self.bproj_coords, order=1)
