d970e33f86615d176d521ea94b143f07bcdf8c33
JKrehl <Jonas.Krehl@triebenberg.de>
2016-01-22 18:05:29 +0100

    forgot what i fixed, oh yeah, now works with GPU stuff

--- Scattering/Algorithms/Projection.py
+++ Scattering/Algorithms/Projection.py
@@ -1,19 +1,27 @@
-from __future__ import absolute_import, division
-
 import numpy
 from ..Operators import OperatorChain
 from ..Operators.TransferFunctions import FlatAtomDW
 from ..Potentials.AtomPotentials import WeickenmeierKohl
 from ...Mathematics import FourierTransforms as FT
+from ...Utilities import Physics
 from ...Utilities import Progress
 
 class Projection:
-	def __init__(self, x, y, potential, energy, zi=None, zf=None, trafo=None, forgetful=False, atom_potential_generator=WeickenmeierKohl, transfer_function=FlatAtomDW):
-		self.__dict__.update(dict(x=x, y=y, energy=energy,
+	def __init__(self, y, x, potential, energy, zi=None, zf=None, trafo=None, forgetful=False,
+	             atom_potential_generator=WeickenmeierKohl, transfer_function=FlatAtomDW, transfer_function_args=None):
+
+		if transfer_function_args is None: transfer_function_args = {}
+
+		self.__dict__.update(dict(y=y, x=x, energy=energy,
 								  zi=zi, zf=zf, trafo=trafo,
 								  forgetful = forgetful,
-								  atom_potential_generator=atom_potential_generator, transfer_function=transfer_function))
+								  atom_potential_generator=atom_potential_generator, transfer_function=transfer_function, transfer_function_args=transfer_function_args))
 		self.prepared = False
+		self.opchain = None
+		self.k = None
+		self.kx = None
+		self.ky = None
+		self.kk = None
 		
 		if self.trafo is not None:
 			self.potential = potential.transform(self.trafo)
@@ -27,15 +35,17 @@ class Projection:
 			self.zi = self.potential.zmin()
 		
 	def prepare(self):
+		self.potential.zsort()
+
 		self.opchain = OperatorChain(zi=self.zi, zf=self.zf)
 
-		phaseshifts_f = {Z:self.atom_potential_generator.phaseshift_f(Z, self.energy, self.x, self.y) for Z in numpy.unique(self.potential.atoms['Z'])}
+		self.k = Physics.wavenumber(self.energy)
 
-		kx, ky = FT.reciprocal_coords(self.x, self.y)
-		kk =  numpy.add.outer(kx**2, ky**2)
+		self.kx, self.ky = FT.reciprocal_coords(self.x, self.y)
+		self.kk =  numpy.add.outer(self.kx**2, self.ky**2)
 		
-		for i in xrange(self.potential.atoms.size):
-			self.opchain.append(self.transfer_function(self.x, self.y, self.potential.atoms[i:i+1], kx=kx, ky=ky, kk=kk, phaseshifts_f=phaseshifts_f, lazy=True, forgetful=self.forgetful))
+		#for i in range(self.potential.atoms.size):
+		self.opchain.append(self.transfer_function.inherit(self, self.potential.atoms))#[i:i+1]))
 
 		self.opchain.impose_zorder()
 		
@@ -54,5 +64,12 @@ class Projection:
 		else:
 			for op in self.opchain['operator']:
 				wave = op.apply(wave)
+
+		if hasattr(wave, 'thread'):
+			thread = wave.thread
+			wave = wave.get()
+			thread.synchronize()
+			thread.release()
+			del thread
 
 		return wave
