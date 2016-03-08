314010e7c93cef2529cd99ccf9e5628be4e95212
JKrehl <Jonas.Krehl@triebenberg.de>
2016-03-08 14:11:57 +0100

    fixed the extent of the images when included in pgfplots

--- Utilities/ImExport.py
+++ Utilities/ImExport.py
@@ -9,9 +9,11 @@ import math
 import decimal
 
 def human_readable_limits(lower, upper):
-	diff = int(math.floor(math.log10((upper-lower)/2)))
+	diff = int(math.floor(numpy.log(upper-lower)-.5))
+
 	nlower = math.floor(decimal.Decimal(lower*10**-diff))*10**decimal.Decimal(diff)
 	nupper = math.ceil(decimal.Decimal(upper*10**-diff))*10**decimal.Decimal(diff)
+
 	return (nlower, nupper)
 
 def imexport(path, a, y, x, norm=SymNorm(), cmap='viridis', clim=None):
@@ -20,17 +22,22 @@ def imexport(path, a, y, x, norm=SymNorm(), cmap='viridis', clim=None):
 
 	if clim is None:
 		sm.autoscale()
+	elif clim=='human':
+		sm.autoscale()
 		sm.set_clim(*human_readable_limits(*sm.get_clim()))
 	else:
 		sm.set_clim(*clim)
 
 	scipy.misc.imsave(path, sm.to_rgba(a))
 
+	hpsx = .5*(x[1]-x[0])
+	hpsy = .5*(y[1]-y[0])
+
 	with open(path+".pars", "w") as file:
-		file.writelines("{}\n".format(x[0]))
-		file.writelines("{}\n".format((x[-1]+x[1]-x[0])))
-		file.writelines("{}\n".format(y[0]))
-		file.writelines("{}\n".format((y[-1]+y[1]-y[0])))
+		file.writelines("{}\n".format(x[0]-hpsx))
+		file.writelines("{}\n".format((x[-1]+hpsx)))
+		file.writelines("{}\n".format(y[0]-hpsy))
+		file.writelines("{}\n".format((y[-1]+hpsy)))
 		file.writelines("{}\n".format(sm.get_clim()[0]))
 		file.writelines("{}\n".format(sm.get_clim()[1]))
 		file.writelines("{}\n".format(sm.get_cmap().name))
